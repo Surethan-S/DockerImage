c06cedcc0578ada413d0b2bda16f7fe657fd6b3f
MAKANESHA <127739640+Makanesha@users.noreply.github.com>
2025-02-05 14:08:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u makanesha -p 041204nesha
-    docker tag test makanesha/guvi
-    docker push makanesha/guvi
+    docker tag test makanesha/guvi:sample
+    docker push makanesha/guvi:sample
     
