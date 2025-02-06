3e4b364c50fbf921f6c1455b7e1669663a97ba97
Surethan-S <127739936+Surethan-S@users.noreply.github.com>
2025-02-06 09:10:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u makanesha -p 041204nesha
-    docker tag test makanesha/guvi:sample
-    docker push makanesha/guvi:sample
+    docker login -u surethan37 -p 55665566@S37
+    docker tag test surethan37/task2:sample
+    docker push surethan37/task2:sample
     
